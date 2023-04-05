eab2d259835a3766b895797ed1df593773e89ce5
Mario <cursotecnoweb@gmail.com>
2023-04-05 21:29:14 +0200

    Added some style to Days's menu

--- view/menu/menu_day_view.php
+++ view/menu/menu_day_view.php
@@ -1,15 +1,16 @@
 <aside class="col-12 col-lg-3 mb-3 menuDia">		
     <div class="row text-center mb-3">
         <div class="col d-flex justify-content-center align-items-center">
-            <h3>Menú del día</h3>
-            <img class="img-fluid" src="/images/menu_dia_logo.png" alt="menu-logo">
-        </div>				
-    </div>		
+            <h2 class="col-8">Menú del día</h2>
+            <img class="col-3 img-fluid" src="/images/menu_dia_logo.png" alt="menu-logo">            
+        </div>        				
+    </div>
+    <hr>		
     <div class="row mb-3">
         <h4 class="text-center"><strong>PRIMEROS PLATOS</strong></h4>
         <ul class="ps-4">
             <?php foreach ($primeros as $key => $plato) { ?>
-                <li><?php echo ucfirst($plato['name']); ?></li>
+                <li><em><?php echo ucfirst($plato['name']); ?></em></li>
             <?php } ?>
         </ul>
     </div>
@@ -17,7 +18,7 @@
         <h4 class="text-center"><strong>SEGUNDOS PLATOS</strong></h4>
         <ul class="ps-4">
             <?php foreach ($segundos as $key => $plato) { ?>
-                <li><?php echo ucfirst($plato['name']); ?></li>
+                <li><em><?php echo ucfirst($plato['name']); ?></em></li>
             <?php } ?>
         </ul>
     </div>
@@ -25,11 +26,12 @@
         <h4 class="text-center"><strong>POSTRE</strong></h4>
         <ul class="ps-4">
             <?php foreach ($postres as $key => $postre) { ?>
-                <li><?php echo ucfirst($postre['name']); ?></li>
+                <li><em><?php echo ucfirst($postre['name']); ?></em></li>
             <?php } ?>
         </ul>
     </div>
-                            
+    <hr>
+
     <h4><strong>PRECIO: <?php echo number_format($menuDayPrice, 2, ",", "."); ?>&nbsp;€</strong></h4>
     <p>*Bebida a elegir, agua, vino o refresco</p>
 </aside>
