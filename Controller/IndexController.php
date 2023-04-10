0e07f89f1724a43cb4a7f9e44c2493d553e003a5
Mario <cursotecnoweb@gmail.com>
2023-04-11 00:42:04 +0200

    Added 'try...catch' block to 'IndexController'

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -16,18 +16,34 @@
        */
         public function index()
         {
-            $menuDayQuery = new QueryMenu();            
+            try {
+                $menuDayQuery = new QueryMenu();            
 
-            $primeros = $menuDayQuery->selectDishesOfDay("primero", $this->dbcon);
-            $segundos = $menuDayQuery->selectDishesOfDay("segundo", $this->dbcon);
-            $postres = $menuDayQuery->selectDishesOfDay("postre", $this->dbcon);
+                $primeros = $menuDayQuery->selectDishesOfDay("primero", $this->dbcon);
+                $segundos = $menuDayQuery->selectDishesOfDay("segundo", $this->dbcon);
+                $postres = $menuDayQuery->selectDishesOfDay("postre", $this->dbcon);
 
 
-            /** Calculate menu's day price */
+                /** Calculate menu's day price */
 
-            $menuDayPrice = $menuDayQuery->getMenuDayPrice($this->dbcon);                      
-           
-            include(SITE_ROOT . "/../view/main_view.php");
+                $menuDayPrice = $menuDayQuery->getMenuDayPrice($this->dbcon); 
+                            
+                            
+                include(SITE_ROOT . "/../view/main_view.php");
+
+            } catch (\Throwable $th) {
+                $error_msg = "<p class='alert alert-danger text-center'>{$th->getMessage()}</p>";
+
+                if(isset($_SESSION['role']) && $_SESSION['role'] === 'ROLE_ADMIN') {
+                    $error_msg = "<p class='alert alert-danger text-center'>
+                                    Message: {$th->getMessage()}<br>
+                                    Path: {$th->getFile()}<br>
+                                    Line: {$th->getLine()}
+                                </p>";
+                }
+
+                include(SITE_ROOT . "/../view/database_error.php");
+            }
         }
     }    
 ?>  
