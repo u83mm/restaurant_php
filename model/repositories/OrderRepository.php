beaa216abeb8c686f93f4073ac6bed95dacfc9ae
Mario <cursotecnoweb@gmail.com>
2023-05-19 21:21:52 +0200

    Improved 'updateOrder' method in 'OrderRepository.php'

--- model/repositories/OrderRepository.php
+++ model/repositories/OrderRepository.php
@@ -99,67 +99,57 @@
                         coffees       = :coffees,
                         coffees_qty   = :coffees_qty
                     WHERE id = :id
-                    ";
+                    ";            
 
-            // CONFIGURAR OPCIÓN DE LIMPIAR LAS COMAS INICIALES EN EL PRIMER ELEMENTO DE CADA PROPIEDAD
-
-            $aperitif_string     = implode(",", $order->getAperitif());
-            $aperitif_qty_string = implode(",", $order->getAperitifQty());
-            $first_string        = implode(",", $order->getFirst());
-            $first_qty_string    = implode(",", $order->getFirstQty());
-            $second_string       = implode(",", $order->getSecond());
-            $second_qty_string   = implode(",", $order->getSecondQty());
-            $dessert_string      = implode(",", $order->getDessert());
-            $dessert_qty_string  = implode(",", $order->getDessertQty());
-            $drink_string        = implode(",", $order->getDrink());
-            $drink_qty_string    = implode(",", $order->getDrinkQty());
-            $coffee_string       = implode(",", $order->getCoffee());
-            $coffee_qty_string   = implode(",", $order->getCoffeeQty());
-
-            if (preg_match('/^,/', $aperitif_string))     $aperitif_string     = ltrim($aperitif_string, ",");
-            if (preg_match('/^,/', $aperitif_qty_string)) $aperitif_qty_string = ltrim($aperitif_qty_string, ",");
-            if (preg_match('/^,/', $first_string))        $first_string        = ltrim($first_string, ",");
-            if (preg_match('/^,/', $first_qty_string))    $first_qty_string    = ltrim($first_qty_string, ",");
-            if (preg_match('/^,/', $second_string))       $second_string       = ltrim($second_string, ",");
-            if (preg_match('/^,/', $second_qty_string))   $second_qty_string   = ltrim($second_qty_string, ",");
-            if (preg_match('/^,/', $dessert_string))      $dessert_string      = ltrim($dessert_string, ",");
-            if (preg_match('/^,/', $dessert_qty_string))  $dessert_qty_string  = ltrim($dessert_qty_string, ",");
-            if (preg_match('/^,/', $drink_string))        $drink_string        = ltrim($drink_string, ",");
-            if (preg_match('/^,/', $drink_qty_string))    $drink_qty_string    = ltrim($drink_qty_string, ",");
-            if (preg_match('/^,/', $coffee_string))       $coffee_string       = ltrim($coffee_string, ",");
-            if (preg_match('/^,/', $coffee_qty_string))   $coffee_qty_string   = ltrim($coffee_qty_string, ",");
-
-            $order->setAperitif(explode(",", $aperitif_string));
-            $order->setAperitifQty(explode(",", $aperitif_qty_string));
-            $order->setFirst(explode(",", $first_string));
-            $order->setFirstQty(explode(",", $first_qty_string));
-            $order->setSecond(explode(",", $second_string));
-            $order->setSecondQty(explode(",", $second_qty_string));
-            $order->setDessert(explode(",", $dessert_string));
-            $order->setDessertQty(explode(",", $dessert_qty_string));
-            $order->setDrink(explode(",", $drink_string));
-            $order->setDrinkQty(explode(",", $drink_qty_string));
-            $order->setCoffee(explode(",", $coffee_string));
-            $order->setCoffeeQty(explode(",", $coffee_qty_string));            
+            $properties = [
+                'aperitif'     => 'Aperitif',
+                'aperitifQty'  => 'AperitifQty',
+                'first'        => 'First',
+                'firstQty'     => 'FirstQty',
+                'second'       => 'Second',
+                'secondQty'    => 'SecondQty',
+                'dessert'      => 'Dessert',
+                'dessertQty'   => 'DessertQty',
+                'drink'        => 'Drink',
+                'drinkQty'     => 'DrinkQty',
+                'coffee'       => 'Coffee',
+                'coffeeQty'    => 'CoffeeQty',
+            ];
+            
+            foreach ($properties as $property => $string) {
+                $value = implode(",", $order->{'get'.$string}());
+                $value = ltrim($value, ",");
+                $order->{'set'.$string}(explode(",", $value));
+            }
+                      
 
             try {
+                $bindings = [                    
+                    'aperitifs'     => $order->getAperitif(),
+                    'aperitifs_qty' => $order->getAperitifQty(),
+                    'firsts'        => $order->getFirst(),
+                    'firsts_qty'    => $order->getFirstQty(),
+                    'seconds'       => $order->getSecond(),
+                    'seconds_qty'   => $order->getSecondQty(),
+                    'desserts'      => $order->getDessert(),
+                    'desserts_qty'  => $order->getDessertQty(),
+                    'drinks'        => $order->getDrink(),
+                    'drinks_qty'    => $order->getDrinkQty(),
+                    'coffees'       => $order->getCoffee(),
+                    'coffees_qty'   => $order->getCoffeeQty(),
+                ];
+                
                 $stm = $dbcon->pdo->prepare($query);
                 $stm->bindValue(":id", $order->getId());
-                $stm->bindValue(":aperitifs",     implode(',', $order->getAperitif()));
-                $stm->bindValue(":aperitifs_qty", implode(',', $order->getAperitifQty()));
-                $stm->bindValue(":firsts",        implode(',', $order->getFirst()));
-                $stm->bindValue(":firsts_qty",    implode(',', $order->getFirstQty()));
-                $stm->bindValue(":seconds",       implode(',', $order->getSecond()));
-                $stm->bindValue(":seconds_qty",   implode(',', $order->getSecondQty()));
-                $stm->bindValue(":desserts",      implode(',', $order->getDessert()));
-                $stm->bindValue(":desserts_qty",  implode(',', $order->getDessertQty()));
-                $stm->bindValue(":drinks",        implode(',', $order->getDrink()));
-                $stm->bindValue(":drinks_qty",    implode(',', $order->getDrinkQty()));
-                $stm->bindValue(":coffees",       implode(',', $order->getCoffee()));
-                $stm->bindValue(":coffees_qty",   implode(',', $order->getCoffeeQty())); 
+
+                foreach ($bindings as $placeholder => $value) {
+                    $value = implode(',', $value);
+                    $stm->bindValue(':' . $placeholder, $value);
+                }
 
                 $stm->execute();       				
-                $stm->closeCursor();                
+                $stm->closeCursor();
+
             } catch (\Throwable $th) {
                 throw new \Exception("{$th->getMessage()}", 1); 
             }
