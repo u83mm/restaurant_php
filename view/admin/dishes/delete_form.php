3b0edf59e047867b97efbc7cd10241d703c91805
Mario <cursotecnoweb@gmail.com>
2023-07-09 14:10:14 +0200

    Translate to english language delete alert

--- view/admin/dishes/delete_form.php
+++ view/admin/dishes/delete_form.php
@@ -1,4 +1,4 @@
-<form class="d-inline" action="#" method="post" onsubmit="return confirm('¿Estás seguro de querer eliminar el registro?')">
+<form class="d-inline" action="#" method="post" onsubmit="return confirm('¿<?php echo ucfirst($page->language['alert_delete']); ?>?')">
     <input type="hidden" name="dishe_id" value="<?php echo $value['dishe_id']; ?>">
     <button type="submit" class="btn btn-outline-danger w-45" name="action" value="delete"><?php echo ucfirst($page->language['delete']); ?></button>
 </form>
