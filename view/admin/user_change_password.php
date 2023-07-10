db765a8fbd9572aa67b27892538573f94fa31595
Mario <cursotecnoweb@gmail.com>
2023-07-10 21:43:29 +0200

    Translate to english language 'Change Password' page view

--- view/admin/user_change_password.php
+++ view/admin/user_change_password.php
@@ -6,19 +6,19 @@
 	$home->do_html_header($home->title, $home->h1, $home->meta_name_description, $home->meta_name_keywords);
 	$home->do_html_nav($home->nav_links, "administration");
 ?>
-	<h3 class="text-center">CHANGE PASSWORD</h3>
+	<h3 class="text-center"><?php echo strtoupper($home->language['change_password']); ?></h3>
     <div class="col-6 mx-auto">
         <?php echo $message = $this->message ?? ""; ?>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> 
             <input type="hidden" name="id_user" value="<?php echo $id_user; ?>">           
             <div class="row mb-3">
-                <label class="col-sm-4 col-form-label" for="password">Password:</label>
+                <label class="col-sm-4 col-form-label" for="password"><?php echo ucfirst($home->language['password']); ?>:</label>
                 <div class="col-sm-6">
                     <input class="form-control" type="password" name="password" id="password" required>
                 </div>                
             </div>
             <div class="row mb-3">
-                <label class="col-sm-4 col-form-label" for="new_password">Repeat Password:</label>
+                <label class="col-sm-4 col-form-label" for="new_password"><?php echo ucfirst($home->language['repeat_password']); ?>:</label>
                 <div class="col-sm-6">
                     <input class="form-control" type="password" name="new_password" id="new_password" required>
                 </div>                
@@ -26,14 +26,14 @@
             <div class="row mb-3">
                 <label class="col-sm-4" for="nome">&nbsp;</label>
                 <div class="col-sm-6">
-                    <input class="btn btn-outline-success" type="submit" name="action" value="Change Password">                    
+                    <button class="btn btn-outline-success" type="submit" name="action" value="change password"><?php echo ucwords($home->language['change_password']); ?></button>                
                 </div>                
             </div>                                                              
         </form>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <input type="hidden" name="id_user" value="<?php echo $id_user; ?>">  
             <input type="hidden" name="action" value="show">
-            <input type="submit" class="btn btn-primary mb-5" value="Volver">
+            <button type="submit" class="btn btn-primary mb-5" value="volver"><?php echo ucfirst($home->language['go_back']); ?></button>
         </form>
     </div>    
 <?php
