8d7238ae6784899bd2c29603cf385d09aa93d559
Mario <cursotecnoweb@gmail.com>
2023-10-18 20:38:05 +0200

    Added textarea field to reservations form

--- view/reservations/reservation_form.php
+++ view/reservations/reservation_form.php
@@ -3,13 +3,13 @@
     <!-- DATE AND TIME-->
     <div class="row mb-0 mb-md-3">
         <div class="col-12 col-md-6">
-            <label class="col-3 col-md-4 col-form-label mb-2 mb-md-0" for="date">Fecha:</label>
+            <label class="col-3 col-md-4 form-label mb-2 mb-md-0" for="date">Fecha:</label>
             <div class="col-8 col-md-5 d-inline-block mb-2 mb-md-0">
                 <input class="form-control" type="date" name="date" id="date" required>
             </div> 
         </div>
         <div class="col-12 col-md-6">
-            <label class="col-3 col-md-4 col-form-label mb-2 mb-md-0" for="time">Hora:</label>
+            <label class="col-3 col-md-4  orm-label mb-2 mb-md-0" for="time">Hora:</label>
             <div class="col-8 col-md-7 d-inline-block mb-2 mb-md-0">
                 <select name="time" id="time" required>
                     <option value=""> - Selecciona -</option>
@@ -21,16 +21,16 @@
         </div>               
     </div> 
     
-     <!-- NAME AND QTY -->
+    <!-- NAME AND QTY -->
     <div class="row mb-0 mb-md-3">       
         <div class="col-12 col-md-6">
-            <label class="col-3 col-md-4 col-form-label mb-2 mb-md-0" for="name">Nombre:</label>
+            <label class="col-3 col-md-4 form-label mb-2 mb-md-0" for="name">Nombre:</label>
             <div class="col-8 col-md-7 d-inline-block mb-2 mb-md-0">
                 <input class="form-control" type="text" name="name" id="name" required>
             </div>        
         </div>        
         <div class="col-12 col-md-6">
-            <label class="col-3 col-md-4 col-form-label mb-2 mb-md-0" for="qty">Personas:</label>
+            <label class="col-3 col-md-4 form-label mb-2 mb-md-0" for="qty">Personas:</label>
             <div class="col-8 col-md-7 d-inline-block mb-2 mb-md-0">
                 <select name="qty" id="qty" required>
                     <option value=""> - Selecciona -</option>
@@ -40,6 +40,17 @@
                 </select>
             </div>
         </div>        
+    </div> 
+
+    <!-- COMMENT -->
+    <div class="row mb-0 mb-md-3">       
+        <div class="col-12">
+            <label class="col-2 col-md-2 form-label mb-2 mb-md-0" for="comment">Comentario:</label>
+            <div class="col-12 col-md-9 d-md-inline-block">
+                <textarea class="form-control" name="comment" id="comment" rows="5" placeholder="Escribe tu comentario"></textarea> 
+            </div>
+                   
+        </div>                      
     </div> 
     
     <div class="row mb-3 mt-5   ">
