8c4db2d0d3c0760d2c10b62fba9a6dc7e9709d43
Mario <cursotecnoweb@gmail.com>
2023-11-29 22:56:30 +0100

    Added cron jobs

--- Application/aplication_fns.php
+++ Application/aplication_fns.php
@@ -1,5 +1,7 @@
 <?php	
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../model/classes/Loader.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../app_config.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/connect.php"); //Descomentar si vamos a usar consultas a DB.		
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/connect.php"); //Descomentar si vamos a usar consultas a DB.	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Cron_jobs/clean_access_log.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Cron_jobs/clean_error_log.php");	
 ?>
