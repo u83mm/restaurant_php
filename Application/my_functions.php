f173b40ae6c6a2b53566e09a737aaac1655330a2
Mario <cursotecnoweb@gmail.com>
2024-03-26 21:32:53 +0100

    Updated router function, added test function in my_functions.php file

--- Application/my_functions.php
+++ Application/my_functions.php
@@ -25,17 +25,15 @@
             $controllerNamePrefix = ucfirst($uri[0]);
             $method = $uri[1];       
         }
-        else if(count($uri) > 2) {
+        else if(count($uri) > 2) {            
             if(!empty($uri) && preg_match('/^([1-9]){1,5}$/', $uri[count($uri) - 1])) {
                 $id = $uri[count($uri) - 1]; 
-                array_pop($uri);                                                             
+                array_pop($uri);                                                     
             }
-            else {
-                foreach ($uri as $key => $value) {
-                    if($key == count($uri) - 2) break;
-                    $route .= $value . "/";            
-                }                     
-            } 
+            foreach ($uri as $key => $value) {
+                if($key == count($uri) - 2) break;
+                $route .= $value . "/";            
+            }                          
 
             $controllerNamePrefix = ucfirst($uri[count($uri) - 2]);
             $method = $uri[count($uri) - 1];                                                       
@@ -44,7 +42,7 @@
         // Build the Controller
         try {
             $controllerRoute = SITE_ROOT . "/../Controller/" . $route;        
-            $controllerName = $controllerNamePrefix . "Controller";                          
+            $controllerName = $controllerNamePrefix . "Controller";                        
            
             require_once($controllerRoute . $controllerName . ".php");                     
                       
