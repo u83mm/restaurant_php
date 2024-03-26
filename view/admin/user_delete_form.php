fd004dc7be4e3ea906ad3dfeb9df871f127f67e7
Mario <cursotecnoweb@gmail.com>
2024-03-26 22:20:23 +0100

    Fix issue on delecting a user

--- view/admin/user_delete_form.php
+++ view/admin/user_delete_form.php
@@ -1,4 +1,4 @@
-<form class="d-inline" action="#" method="post" onsubmit="return confirm('¿<?php echo ucfirst($page->language['alert_delete']); ?>?')">
+<form class="d-inline" action="/admin/admin/delete" method="post" onsubmit="return confirm('¿<?php echo ucfirst($page->language['alert_delete']); ?>?')">
     <input type="hidden" name="id_user" value="<?php echo $value['id']; ?>">
     <button type="submit" class="btn btn-outline-danger" name="action" value="delete"><?php echo ucfirst($page->language['delete']); ?></button>
 </form>
