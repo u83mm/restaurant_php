9eac88d44462bb1e027f0cb5f37e824d279a2eb1
Mario <cursotecnoweb@gmail.com>
2024-03-29 18:34:01 +0100

    Changed router implementation from functional programming to POO

--- Application/my_functions.php
+++ Application/my_functions.php
@@ -7,62 +7,7 @@
         echo "</pre>";
 
         die();
-    }
-
-    /** Router function */
-    function router(array $uri = null) : void {
-        $controllerNamePrefix = ucfirst("index");
-        $method = "showCaptcha"; 
-        $route = "";
-        global $id;        
-        
-        // Test diferent options to configure to Controller                 
-        if(count($uri) == 1 && !empty($uri[0])){
-            $controllerNamePrefix = ucfirst($uri[0]);
-            $method = "index";
-        }
-        else if(count($uri) == 2) {
-            $controllerNamePrefix = ucfirst($uri[0]);
-            $method = $uri[1];       
-        }
-        else if(count($uri) > 2) {            
-            if(!empty($uri) && preg_match('/^([1-9]){1,5}$/', $uri[count($uri) - 1])) {
-                $id = $uri[count($uri) - 1]; 
-                array_pop($uri);                                                     
-            }
-            foreach ($uri as $key => $value) {
-                if($key == count($uri) - 2) break;
-                $route .= $value . "/";            
-            }                          
-
-            $controllerNamePrefix = ucfirst($uri[count($uri) - 2]);
-            $method = $uri[count($uri) - 1];                                                       
-        }  
-        
-        // Build the Controller
-        try {
-            $controllerRoute = SITE_ROOT . "/../Controller/" . $route;        
-            $controllerName = $controllerNamePrefix . "Controller";                        
-           
-            require_once($controllerRoute . $controllerName . ".php");                     
-                      
-            $controller = new $controllerName;                       
-            $controller->$method();                         
-    
-        } catch (\Throwable $th) {
-            $error_msg = "<p class='alert alert-danger text-center'>Page not found</p>";
-    
-            if(isset($_SESSION['role']) && $_SESSION['role'] === 'ROLE_ADMIN') {
-                $error_msg = "<p class='alert alert-danger text-center'>
-                                Message: {$th->getMessage()}<br>
-                                Path: {$th->getFile()}<br>
-                                Line: {$th->getLine()}
-                            </p>";
-            }
-    
-            include(SITE_ROOT . "/../view/database_error.php");
-        }
-    }
+    }    
 
     /** Test access */
     function testAccess() : void {
--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
-	declare(strict_types=1);	
+	declare(strict_types=1);
+
+    use model\classes\App;
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/aplication_fns.php");		
 
     /** Test page language */
 	$_SESSION['language'] = $_POST['language'] ?? $_SESSION['language'] ?? "spanish";                   
 
-    $uri = explode("/", PATH);
+    /* $uri = explode("/", PATH);
     array_shift($uri);          
 
-    router($uri);      
+    router($uri); */
+    
+    $app = new App();
+    $app->router();
 ?>
