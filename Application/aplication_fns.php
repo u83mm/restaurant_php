8fec0f078d805c63866ffe606694cb1732c03d55
Mario <cursotecnoweb@gmail.com>
2024-05-07 20:43:44 +0200

    Updated .gitignore file

--- Application/aplication_fns.php
+++ Application/aplication_fns.php
@@ -2,7 +2,11 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/my_functions.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/model/classes/Loader.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/app_config.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/connect.php"); //Descomentar si vamos a usar consultas a DB.	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/connect.php"); 
+
+	### API KEYS
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/api_keys.php");
+	
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Cron_jobs/clean_access_log.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/../Application/Cron_jobs/clean_error_log.php");	
 ?>
--- Application/app_config.php
+++ Application/app_config.php
@@ -7,10 +7,7 @@
 
 	/** Establece número máximo de dígitos para numerar las mesas y las personas por mesa */
 	define("MAX_DIGITS_TO_TABLE_NUMBERS", 2);
-	define("MAX_DIGITS_TO_PEOPLE_QTY", 2);
-
-	/** API KEY to send emails with Resend API */
-	define('RESEND_API_KEY', 're_8E64TxAJ_2Ft8a1HqTdLiKdonsvtdB6QG');
+	define("MAX_DIGITS_TO_PEOPLE_QTY", 2);	
 
 	/** Define connection */
 	require_once(SITE_ROOT . "/../Application/connect.php");
