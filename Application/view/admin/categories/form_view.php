e593acee437ff8239988df50c70160ca2d2f2e26
Mario Moreno <cursotecnoweb@gmail.com>
2025-02-12 18:43:38 +0100

    Implement save a new dish category

---
+++ Application/view/admin/categories/form_view.php
@@ -0,0 +1,26 @@
+<form id="categories-form" action="/admin/categories/new" method="post">
+    <?php extract($fields); ?>
+    <div class="mb-3">
+        <label for="category" class="form-label col-md-3"><?php echo ucfirst($page->language['category']); ?>:</label>
+        <div class="col-md-7 d-inline-block">
+            <input type="text" class="form-control" id="category" name="category" value="<?php if(isset($category)) echo $category; ?>" required>
+        </div>                
+    </div>
+    <div class="mb-3">
+        <label for="dish_menu_menuEmoji" class="form-label col-md-3">Emoji:</label>
+        <div class="col-md-7 d-inline-block">
+            <input type="text" class="form-control" id="dish_menu_menuEmoji" name="emoji" value="<?php echo $emoji ?? '' ?>" readonly>
+        </div>                
+    </div>
+    <div class="mb-3 d-md-flex">
+        <label class="col col-md-3 align-self-baseline align-self-md-center mt-0 pt-0" for="none"><?php echo ucfirst($page->language['select_emoji']) ?>:</label>
+        <div id="emoji-container" class="col col-md-7"></div>
+    </div>
+    <div class="mb-3">
+        <label class="col-lg-3" for="none">&nbsp;</label>
+        <div class="col-lg-7 d-inline-block">
+            <button type="submit" class="btn btn-outline-primary mt-0"><?php echo ucfirst($page->language['save']); ?></button>
+            <a class="btn btn-outline-primary mb-5" href="/admin/categories/index"><?php echo ucfirst($page->language['go_back']); ?></a>
+        </div>                
+    </div>
+</form>
